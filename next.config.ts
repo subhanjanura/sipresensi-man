2abc612c37e4f756d2ba7c31790dd28b748b2c31
subhanjanura <subhanjanura24@gmail.com>
2026-08-02 12:09:34 +0700

    Setup project successfully

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-<<<<<<< HEAD
   reactCompiler: true,
-=======
->>>>>>> 759dfb3880b55c25bc7d5f4bffe9be4f4a5b37f1
 };
 
 export default nextConfig;
